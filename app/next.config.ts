e21107747be6702314ca7283413c4c2b867a575e
adventistascajazeira8 <adventistascajazeira8@gmail.com>
2026-04-20 21:56:53 -0300

    teste deply

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 1. Tente na raiz diretamente (como o erro sugere)
+  allowedDevOrigins: ['192.168.100.43'],
+
+  // 2. Mantenha aqui também por redundância de versão
+  devIndicators: {
+    allowedDevOrigins: ['192.168.100.43'],
+  },
 };
 
 export default nextConfig;
